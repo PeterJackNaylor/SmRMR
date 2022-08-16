7d6e3022bd0d05cb87438ae8035907aa95cdfe4c
PeterJackNaylor <peter.jack.naylor@gmail.com>
2022-08-16 11:59:56 +0900

    fix precompute kernel and reduce the main lamda_control file

---
+++ src/templates/lambda_control/lambda_control_utils.py
@@ -0,0 +1,109 @@
+from functools import partial
+import numpy as np
+from dclasso.dc_lasso import alpha_threshold, loss, minimize_loss
+from dclasso import pic_penalty
+
+
+def fdr(causal_features, selected_features):
+
+    if len(selected_features) == 0:
+        print("No feature selection process happened")
+        return -1
+    else:
+        n_selected = len(selected_features)
+        intersection = list(set(selected_features) & set(causal_features))
+        number_of_correct_positives = len(intersection)
+        fdr = (n_selected - number_of_correct_positives) / n_selected
+        return fdr
+
+
+def perform_alpha_computations(alpha_list, wjs, screen_indices, causal_features):
+    run_fdr = []
+    run_var_selected = []
+    for alpha in alpha_list:
+
+        selected_features, _, _ = alpha_threshold(alpha, wjs, screen_indices)
+        selected_features = list(np.array(selected_features))
+        run_fdr.append(fdr(causal_features, selected_features))
+        run_var_selected.append(selected_features)
+    return run_fdr, run_var_selected
+
+
+def perform_optimisation_with_parameters(
+    dclasso_main,
+    pen,
+    opt,
+    lam,
+    alpha_list,
+    d,
+    causal_features,
+    key,
+    max_epoch,
+    eps_stop,
+    opt_kwargs,
+    Cst,
+    penalty_kwargs,
+):
+    penalty_kwargs = {"name": pen, "lamb": lam}
+    loss_fn = partial(
+        loss,
+        Dxy=dclasso_main.Dxy,
+        Dxx=dclasso_main.Dxx,
+        penalty_func=pic_penalty(penalty_kwargs),
+    )
+
+    step_function, opt_state, beta = dclasso_main.setup_optimisation(
+        loss_fn,
+        opt,
+        d,
+        key,
+        "from_convex_solve",
+        dclasso_main.Dxx,
+        dclasso_main.Dxy,
+        opt_kwargs,
+    )
+    beta, _ = minimize_loss(
+        step_function,
+        opt_state,
+        beta,
+        max_epoch,
+        eps_stop,
+        verbose=dclasso_main.verbose,
+    )
+    wjs = beta[:d] - beta[d:]
+    fdr_l, selected_l = perform_alpha_computations(
+        alpha_list, wjs, dclasso_main.screen_indices_, causal_features
+    )
+
+    loss_fn = partial(
+        loss,
+        Dxy=dclasso_main.Dxy,
+        Dxx=dclasso_main.Dxx,
+        penalty_func=pic_penalty({"name": "None"}),
+    )
+
+    loss_train = float(loss_fn(beta))
+    loss_fn = partial(
+        loss,
+        Dxy=dclasso_main.Dxy_val,
+        Dxx=dclasso_main.Dxx_val,
+        penalty_func=pic_penalty({"name": "None"}),
+    )
+    loss_valid = float(loss_fn(beta[:d]))
+    if pen != "None":
+        R = float(Cst / penalty_kwargs["lamb"] * pic_penalty(penalty_kwargs)(beta))
+    else:
+        R = 0.0
+    N1 = np.abs(beta).sum()
+
+    return fdr_l, selected_l, loss_train, loss_valid, R, N1
+
+
+def build_iterator(penalty_list, optimizer_list, lambdas_list):
+    for opt in optimizer_list:
+        for pen in penalty_list:
+            if pen == "None":
+                yield pen, opt, 0
+            else:
+                for lam in lambdas_list:
+                    yield pen, opt, lam
--- src/templates/lambda_control/main.py
+++ src/templates/lambda_control/main.py
@@ -11,8 +11,8 @@ Input variables:
 Output files:
     - performance.tsv: path to a single-line tsv with the TSV results.
 """
-import yaml
 from functools import partial
+import yaml
 import numpy as np
 from jax import random
 import jax.numpy as jnp
@@ -25,10 +25,15 @@ except ImportError:
     from scipy.sparse.linalg import ArpackNoConvergence
 
 from dclasso import DCLasso, pic_penalty
-from dclasso.dc_lasso import alpha_threshold, loss, minimize_loss
+from dclasso.dc_lasso import loss
 
-# from dclasso.utils import get_equi_features
 import utils as u
+from lambda_control_utils import (
+    build_iterator,
+    fdr,
+    perform_alpha_computations,
+    perform_optimisation_with_parameters,
+)
 
 key = random.PRNGKey(42)
 Cst = 1.5
@@ -39,100 +44,6 @@ opt_kwargs = {
     "transition_steps": 100,
     "decay_rate": 0.99,
 }
-
-
-def fdr(causal_features, selected_features):
-
-    if len(selected_features) == 0:
-        print("No feature selection process happened")
-        return -1
-    else:
-        n_selected = len(selected_features)
-        intersection = list(set(selected_features) & set(causal_features))
-        number_of_correct_positives = len(intersection)
-        fdr = (n_selected - number_of_correct_positives) / n_selected
-        return fdr
-
-
-def perform_alpha_computations(alpha_list, wjs, screen_indices, causal_features):
-    run_fdr = []
-    run_var_selected = []
-    for alpha in alpha_list:
-
-        selected_features, _, _ = alpha_threshold(alpha, wjs, screen_indices)
-        selected_features = list(np.array(selected_features))
-        run_fdr.append(fdr(causal_features, selected_features))
-        run_var_selected.append(selected_features)
-    return run_fdr, run_var_selected
-
-
-def perform_optimisation_with_parameters(
-    dclasso_main, pen, opt, lam, alpha_list, d, causal_features
-):
-    penalty_kwargs = {"name": pen, "lamb": lam}
-    loss_fn = partial(
-        loss,
-        Dxy=dclasso_main.Dxy,
-        Dxx=dclasso_main.Dxx,
-        penalty_func=pic_penalty(penalty_kwargs),
-    )
-
-    step_function, opt_state, beta = dclasso_main.setup_optimisation(
-        loss_fn,
-        opt,
-        d,
-        key,
-        "from_convex_solve",
-        dclasso_main.Dxx,
-        dclasso_main.Dxy,
-        opt_kwargs,
-    )
-    beta, _ = minimize_loss(
-        step_function,
-        opt_state,
-        beta,
-        max_epoch,
-        eps_stop,
-        verbose=dclasso_main.verbose,
-    )
-    wjs = beta[:d] - beta[d:]
-    fdr_l, selected_l = perform_alpha_computations(
-        alpha_list, wjs, dclasso_main.screen_indices_, causal_features
-    )
-
-    loss_fn = partial(
-        loss,
-        Dxy=dclasso_main.Dxy,
-        Dxx=dclasso_main.Dxx,
-        penalty_func=pic_penalty({"name": "None"}),
-    )
-
-    loss_train = float(loss_fn(beta))
-    loss_fn = partial(
-        loss,
-        Dxy=dclasso_main.Dxy_val,
-        Dxx=dclasso_main.Dxx_val,
-        penalty_func=pic_penalty({"name": "None"}),
-    )
-    loss_valid = float(loss_fn(beta[:d]))
-    if pen != "None":
-        R = float(Cst / penalty_kwargs["lamb"] * pic_penalty(penalty_kwargs)(beta))
-    else:
-        R = 0.0
-    N1 = np.abs(beta).sum()
-
-    return fdr_l, selected_l, loss_train, loss_valid, R, N1
-
-
-def build_iterator(penalty_list, optimizer_list, lambdas_list):
-    for opt in optimizer_list:
-        for pen in penalty_list:
-            if pen == "None":
-                yield pen, opt, 0
-            else:
-                for lam in lambdas_list:
-                    yield pen, opt, lam
-
 
 am_kernels = ["HSIC", "cMMD"]
 
@@ -206,7 +117,14 @@ dl.fit(
     penalty_kwargs=penalty_kwargs,
     optimizer=optimizer[0],
 )
-loss_train__ = float(dl.final_loss_)
+loss_fn = partial(
+    loss,
+    Dxy=dl.Dxy,
+    Dxx=dl.Dxx,
+    penalty_func=pic_penalty({"name": "None"}),
+)
+
+loss_train__ = float(loss_fn(dl.beta_))
 selected_ = list(np.array(dl.alpha_indices_))
 fdr_ = fdr(causal_feats, selected_)
 
@@ -219,6 +137,10 @@ X_val = X_val[:, dl.screen_indices_]
 # Xhat = get_equi_features(X_val, key)
 # X_val = jnp.concatenate([X_val, Xhat], axis=1)
 y_val = jnp.array(y_val)
+
+if "precompute" in dl.ms_kwargs.keys():
+    del dl.ms_kwargs["precompute"]
+
 Dxy_val = dl._compute_assoc(X_val, y_val, **dl.ms_kwargs)
 Dxx_val = dl._compute_assoc(X_val, **dl.ms_kwargs)
 dl.Dxy_val = Dxy_val
