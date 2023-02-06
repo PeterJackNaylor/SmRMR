18ed8dc9569d117a0675f305dda09a22df67df7d
PeterJackNaylor <peter.jack.naylor@gmail.com>
2023-02-06 16:10:07 +0900

    did some modifications, added tqdm, removed non zero feature feature

--- dclasso/dc_lasso.py
+++ dclasso/dc_lasso.py
@@ -1,27 +1,30 @@
 from functools import partial
 from collections.abc import Callable
 import numpy.typing as npt
-from tqdm import trange
-import numpy as npy
 import jax.numpy as np
 from jax.lax import top_k
-from jax import jit, vmap, value_and_grad, random
+from jax import value_and_grad, random
 
 import optax
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.utils.validation import check_X_y
 
 from . import association_measures as am
-from .association_measures.hsic import precompute_kernels
-from .association_measures.distance_correlation import pdist_p, fill_diagonal
+
 from .penalties import pic_penalty
 from .utils import (
     knock_off_check_parameters,
     get_equi_features,
     generate_random_sets,
+    pos_proj,
+    get_optimizer,
+    minimize_loss,
+    selected_top_k,
+    precompute_kernels_match,
+    alpha_threshold,
+    gene_generators,
 )
 
-from optax._src.base import GradientTransformation
 
 # General comments
 # - Private methods should be named __method_name
@@ -105,7 +108,7 @@ class DCLasso(BaseEstimator, TransformerMixin):
         },
         conservative: bool = True,
     ):
-        self.verbose = True
+
         key = random.PRNGKey(seed)
 
         X, y = check_X_y(X, y)
@@ -241,7 +244,7 @@ class DCLasso(BaseEstimator, TransformerMixin):
         self.n_features_in_ = p
 
         generator_ms_kernel, hyperparameter_generator = gene_generators(
-            param_grid, self.measure_stat, self.kernel
+            param_grid, self.measure_stat, self.kernel, kernel_ms
         )
 
         dict_scores = {}
@@ -512,40 +515,40 @@ class DCLasso(BaseEstimator, TransformerMixin):
         screened_indices = selected_top_k(beta, d)
         return screened_indices
 
-    def feature_feature_screen_nonzeros(self, X, y, d, penalty, key):
+    # def feature_feature_screen_nonzeros(self, X, y, d, penalty, key):
 
-        Dxy = self._compute_assoc(X, y, **self.ms_kwargs)
-        Dxx = self._compute_assoc(X, **self.ms_kwargs)
+    #     Dxy = self._compute_assoc(X, y, **self.ms_kwargs)
+    #     Dxx = self._compute_assoc(X, **self.ms_kwargs)
 
-        # Default parameters
-        max_epoch = 150
-        eps_stop = 1e-8
-        init = "from_convex_solve"
-        penalty_kwargs = {"name": f"{penalty}", "lamb": 0.5}
-        optimizer = "adam"
-        opt_kwargs = {
-            "init_value": 0.001,
-            "transition_steps": 100,
-            "decay_rate": 0.99,
-        }
-        loss_fn = partial(
-            loss, Dxy=Dxy, Dxx=Dxx, penalty_func=pic_penalty(penalty_kwargs)
-        )
-        step_function, opt_state, beta = self.setup_optimisation(
-            loss_fn, optimizer, X.shape[1], key, init, Dxx, Dxy, opt_kwargs
-        )
-        beta, _ = minimize_loss(
-            step_function,
-            opt_state,
-            beta,
-            max_epoch,
-            eps_stop,
-            verbose=self.verbose,
-        )
+    #     # Default parameters
+    #     max_epoch = 150
+    #     eps_stop = 1e-8
+    #     init = "from_convex_solve"
+    #     penalty_kwargs = {"name": f"{penalty}", "lamb": 0.5}
+    #     optimizer = "adam"
+    #     opt_kwargs = {
+    #         "init_value": 0.001,
+    #         "transition_steps": 100,
+    #         "decay_rate": 0.99,
+    #     }
+    #     loss_fn = partial(
+    #         loss, Dxy=Dxy, Dxx=Dxx, penalty_func=pic_penalty(penalty_kwargs)
+    #     )
+    #     step_function, opt_state, beta = self.setup_optimisation(
+    #         loss_fn, optimizer, X.shape[1], key, init, Dxx, Dxy, opt_kwargs
+    #     )
+    #     beta, _ = minimize_loss(
+    #         step_function,
+    #         opt_state,
+    #         beta,
+    #         max_epoch,
+    #         eps_stop,
+    #         verbose=self.verbose,
+    #     )
 
-        nonzeros = np.where(beta != 0)[0]
-        screened_indices = beta.argsort()[-nonzeros.shape[0] :][::-1]
-        return screened_indices
+    #     nonzeros = np.where(beta != 0)[0]
+    #     screened_indices = beta.argsort()[-nonzeros.shape[0] :][::-1]
+    #     return screened_indices
 
     def screen_split(self, X, y, n, p, n1, d, penalty, key):
         """
@@ -557,6 +560,7 @@ class DCLasso(BaseEstimator, TransformerMixin):
         if stop:
             raise ValueError(msg)
 
+        screened_indices = np.arange(p)
         if screening:
             # 1 - pre-screening by only checking the marginals
             if self.verbose:
@@ -568,30 +572,33 @@ class DCLasso(BaseEstimator, TransformerMixin):
             X2, y2 = X[s2, :], y[s2]
 
             # if too many features, select the top K = 4 * d
+            # First marginal screening
             # TODO rename d=? and p=n_features?
-            screened_indices = np.arange(p)
+
             if 4 * d < p:
                 screened_indices = self.marginal_screen(X1, y1, 4 * d)
                 X1 = X1[:, screened_indices]
                 X2 = X2[:, screened_indices]
 
             # TODO what does this do?
+            # Second screening in order to reduce even further the number of features
+            # this time, it takes into account feature feature relations
             screened_indices_2 = self.feature_feature_screen(X2, y2, d, penalty, key)
             X1 = X1[:, screened_indices_2]
             X2 = X2[:, screened_indices_2]
             screened_indices = screened_indices[screened_indices_2]
 
         else:
-            if self.verbose:
-                print("Only non-zero screening")
-            # screened_indices = np.arange(p)
-            # TODO same as above
-            screened_indices = self.feature_feature_screen_nonzeros(
-                X, y, d, penalty, key
-            )
+            # if self.verbose:
+            #     print("Only non-zero screening")
+            # # screened_indices = np.arange(p)
+            # # TODO same as above
+            # screened_indices = self.feature_feature_screen_nonzeros(
+            #     X, y, d, penalty, key
+            # )
             d = screened_indices.shape[0]
 
-            X = X[:, screened_indices]
+            # X = X[:, screened_indices]
 
             X1, y1 = np.zeros(shape=(0, d)), np.zeros(shape=(0,))
             X2, y2 = X, y
@@ -618,341 +625,7 @@ class DCLasso(BaseEstimator, TransformerMixin):
         return loss_fn
 
 
-def precompute_kernels_match(measure_stat, X, y, kernel, ms_kwargs, normalise_input):
-    if measure_stat in ["HSIC", "DC"] and normalise_input:
-        X = X / np.linalg.norm(X, ord=2, axis=0)
-
-    match measure_stat:
-        case "HSIC" | "cMMD":
-            ms_kwargs["precompute"] = compute_kernels_for_am(X, y, kernel, **ms_kwargs)
-        case "DC":
-            ms_kwargs["precompute"] = compute_distance_for_am(X, y, **ms_kwargs)
-    return ms_kwargs
-
-
-def alpha_threshold(
-    alpha,
-    wjs,
-    indices,
-    hard_alpha=True,
-    alpha_increase=0.05,
-    return_alpha=False,
-    verbose=True,
-    conservative=True,
-):
-    """
-    Computes the selected features with respect to alpha.
-    Parameters
-    ----------
-    alpha : threshold value to use for post inference selection.
-    Returns
-    -------
-    A 3 element tuple where:
-        1 - indices corresponding to indexes of the chosen features in
-        the original array.
-        2 - the threshold value.
-        3 - the number of selected features.
-    """
-    init_alpha = alpha
-    n_out = []
-    while not n_out and alpha < 1.0:
-        alpha_indices_, t_alpha_ = threshold_alpha(
-            wjs, indices, alpha, conservative, verbose
-        )
-        n_features_out_ = len(alpha_indices_)
-
-        n_out = list(alpha_indices_)
-        if not n_out:
-            alpha += alpha_increase
-        if hard_alpha:
-            break
-
-    if verbose:
-        if len(alpha_indices_):
-            print("selected features: ", alpha_indices_)
-            print(f"init alpha: {init_alpha}; last alpha: {alpha}")
-        else:
-            print("No features were selected, returning empty set.")
-    if return_alpha:
-        return alpha_indices_, t_alpha_, n_features_out_, alpha
-    return alpha_indices_, t_alpha_, n_features_out_
-
-
-def apply_at(fn, pos_lst, iterable):
-    """
-    Apply function at given indices in the iterable.
-
-    Args:
-        fn (function): Function to apply
-        pos_lst list: list-like array containing the indices
-        iterable list: array on which to apply the transformation
-
-    Returns:
-        list: with elements in pos_lst modified by fn.
-    """
-    pos_lst = set(pos_lst)
-    return [fn(x) if i in pos_lst else x for (i, x) in enumerate(iterable)]
-
-
-def pos_proj(array):
-    """Positive projection of the array
-    by simple positive clipping.
-
-    Args:
-        array (numpy array like): input aray
-
-    Returns:
-        Same type as input: clipped array
-    """
-    return array.clip(0)
-
-
 def loss(b, Dxy, Dxx, penalty_func):
     xy_term = -(b * Dxy).sum()
     xx_term = 0.5 * (b * (Dxx * b).T).sum()
     return xy_term + xx_term + penalty_func(b)
-
-
-def minimize_loss(
-    step_function, opt_state, beta, max_epoch, eps_stop, patience=5, verbose=False
-):
-    # error_tmp = []
-    prev = np.inf
-    i = 0
-    # Minimizing loss function
-    range_epoch = trange(max_epoch) if verbose else range(max_epoch)
-    for _ in range_epoch:
-        value, beta, opt_state = step_function(beta, opt_state)
-        # error_tmp.append(float(value))
-        if abs(value - prev) < eps_stop / patience:
-            i += 1
-            if i == patience:
-                break
-        else:
-            i = 0
-        prev = value
-    return beta, value
-
-
-def get_optimizer(opt, opt_kwargs):
-    scheduler = optax.exponential_decay(**opt_kwargs)
-
-    match opt:
-        case "SGD":
-            optimizer = optax.chain(
-                optax.identity(),
-                optax.scale_by_schedule(scheduler),
-                optax.scale(-1.0),
-                optax.keep_params_nonnegative(),
-            )
-
-        case "adam":
-            # Combining gradient transforms using `optax.chain`.
-            optimizer = optax.chain(
-                optax.scale_by_adam(),  # Use the updates from adam.
-                optax.scale_by_schedule(
-                    scheduler
-                ),  # Use the learning rate from the scheduler.
-                # Scale updates by -1 since optax.apply_updates is additive and we
-                # want to descend on the loss.
-                optax.scale(-1.0),
-                optax.keep_params_nonnegative(),
-            )
-        case opt if opt.__class__ == GradientTransformation:
-            optimizer = opt
-        case _:
-            error_msg = f"Unkown optimizer: {opt}, should be a known string or of \
-            GradientTransformation class of the optax python package"
-            raise ValueError(error_msg)
-
-    return optimizer
-
-
-def compute_kernels_for_am(X, y, kernel, **kwargs):
-    _, p = X.shape
-    # mostly for HSIC and cMMD
-    indicesX = np.arange(p)
-    indicesY = np.arange(1)
-
-    def jit_precompute_kernels(x):
-        return precompute_kernels(x, kernel=kernel, **kwargs)
-
-    @jit
-    def precompX(k):
-        return jit_precompute_kernels(X[:, k])
-
-    @jit
-    def precompY(k):
-        return jit_precompute_kernels(y)
-
-    Kx = vmap(precompX)(indicesX)
-    Ky = vmap(precompY)(indicesY)
-    return Kx, Ky
-
-
-def compute_distance_for_am(X, y, **kwargs):
-    # mostly for DC
-    _, p = X.shape
-    indicesX = np.arange(p)
-    indicesY = np.arange(1)
-    p = kwargs["order_x"] if "order_x" in kwargs.keys() else 2
-    q = kwargs["order_x"] if "order_y" in kwargs.keys() else 2
-
-    def pdist_A(x, y, p, unbiased=False):
-        n = X.shape[0]
-        A = pdist_p(x, y, p)
-        if unbiased:
-            A_1 = np.tile(A.sum(axis=1), (n, 1)) / (n - 2)
-            A_0 = np.tile(A.sum(axis=0), (1, n)).reshape(n, n, order="F") / (n - 2)
-            A = A - A_0 - A_1 + A.sum() / ((n - 1) * (n - 2))
-            A = fill_diagonal(A, 0)
-        else:
-            A_1 = np.tile(A.mean(axis=1), (n, 1))
-            A_0 = np.tile(A.mean(axis=0), (1, n)).reshape(n, n, order="F")
-            mean = A.mean()
-            A = A - A_0 - A_1 + mean
-        return A
-
-    def jit_precompute_dist_x(x):
-        return pdist_A(x, x, p=p)
-
-    def jit_precompute_dist_y(y):
-        return pdist_A(y, y, p=q)
-
-    @jit
-    def precompX(k):
-        return jit_precompute_dist_x(X[:, k])
-
-    @jit
-    def precompY(k):
-        return jit_precompute_dist_y(y)
-
-    Kx = vmap(precompX)(indicesX)
-    Ky = vmap(precompY)(indicesY)
-    return Kx, Ky
-
-
-def threshold_alpha(Ws, w_indice, alpha, conservative=True, verbose=True):
-    """
-    Computes the set defined by equation 3.8
-    Parameters
-    ----------
-    Ws : list like object corresponding to the estimator W_j
-        for each feature.
-    w_indice : numpy array like object corresponding to the indices
-    of the W_j in the original dataset.
-    alpha : float between 0 and 1. Sets the FDR rate.
-    Returns
-    -------
-    indices : numpy array like corresponding to the selected features.
-    t_alpha : float, which is the threshold used to select the set of active
-    features.
-    """
-
-    ts = np.sort(abs(Ws))
-
-    add = 1 if conservative else 0
-
-    def fraction_3_6(t):
-        num = (Ws <= -abs(t)).sum() + add
-        den = max((Ws >= abs(t)).sum(), 1)
-        return num / den
-
-    fraction_3_6_v = npy.vectorize(fraction_3_6)
-    fdp = fraction_3_6_v(ts)
-
-    t_alpha = np.where(fdp <= alpha)
-    if t_alpha[0].size == 0:
-        # no one selected..
-        if verbose:
-            print("alpha_min set to np.inf, no one selected...")
-        t_alpha_min = np.inf
-    else:
-        t_alpha_min = min(ts[t_alpha])
-    indices = w_indice[Ws >= t_alpha_min]
-    ########################################
-    # For debugging purposes
-    # def numerator(t):
-    #    num = (Ws <= -abs(t)).sum() + 1
-    #    return num
-
-    # def denominator(t):
-    #    den = max((Ws >= abs(t)).sum(), 1)
-    #    return den
-
-    # nume_v = npy.vectorize(numerator)
-    # denomi_3_6_v = npy.vectorize(denominator)
-    # nn = nume_v(ts)
-    # dd = denomi_3_6_v(ts)
-    #######################################
-
-    return indices, t_alpha_min
-
-
-def gene_generators(param_grid, measure_stat, kernel):
-    def ms_kernel_gen(param_grid, measure_stat, kernel):
-        if "measure_stat" in param_grid:
-            measure_stat = param_grid["measure_stat"]
-        else:
-            measure_stat = [measure_stat]
-        if "kernel" in param_grid:
-            kernel = param_grid["kernel"]
-        else:
-            kernel = [kernel]
-        for ms in measure_stat:
-            if ms in kernel_ms:
-                for k in kernel:
-                    yield (ms, k)
-            else:
-                yield (ms, kernel[0])
-
-    def hp_gen(param_grid):
-        if "lambda" not in param_grid:
-            lambda_ = [0.5]
-        else:
-            lambda_ = param_grid["lambda"]
-
-        if "penalty" not in param_grid:
-            penalty = ["l1"]
-        else:
-            penalty = param_grid["penalty"]
-            if not isinstance(penalty, list):
-                penalty = [penalty]
-
-        if "learning_rate" not in param_grid:
-            learning_rate = [0.001]
-        else:
-            learning_rate = param_grid["learning_rate"]
-
-        if "optimizer" not in param_grid:
-            optimizer = ["adam"]
-        else:
-            optimizer = param_grid["optimizer"]
-            if not isinstance(optimizer, list):
-                optimizer = [optimizer]
-
-        dic = {}
-        dic["penalty_kwargs"] = {}
-        dic["opt_kwargs"] = {"transition_steps": 100, "decay_rate": 0.99}
-        for pen in penalty:
-            dic["penalty_kwargs"]["name"] = pen
-            for lr in learning_rate:
-                dic["opt_kwargs"]["init_value"] = lr
-                for opt in optimizer:
-                    dic["optimizer"] = opt
-                    for lam in lambda_:
-                        dic["penalty_kwargs"]["lamb"] = lam
-                        if (
-                            pen == "None"
-                            and dic["penalty_kwargs"]["lamb"] == lambda_[0]
-                        ):
-                            dic["penalty_kwargs"]["lamb"] = 0
-                            yield dic
-                        elif pen != "None" and dic["penalty_kwargs"]["lamb"] != 0:
-                            yield dic
-
-    return ms_kernel_gen(param_grid, measure_stat, kernel), hp_gen(param_grid)
-
-
-def selected_top_k(beta, d):
-    return np.argsort(beta)[-d:][::-1]
